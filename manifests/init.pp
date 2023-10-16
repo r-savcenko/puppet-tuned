4288f94936a9b95ac754bca82c2bcfd384b84b45
r-savcenko <ruslan.savcenko@gmail.com>
2023-10-16 12:20:52 +0300

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@ class tuned (
   Integer $sleep_interval                                        = $tuned::params::sleep_interval,
   Integer $update_interval                                       = $tuned::params::update_interval,
   String $majversion                                             = $tuned::params::majversion,
-  Variant[Stdlib::Compat::Absolute_path, String[0,0]] $main_conf = $tuned::params::main_conf,
+  Variant[Stdlib::Absolutepath, String[0,0]] $main_conf = $tuned::params::main_conf,
   Stdlib::Absolutepath $profiles_path                            = $tuned::params::profiles_path,
   String $active_profile_conf                                    = $tuned::params::active_profile_conf,
   Array[String, 1] $packages                                     = $tuned::params::packages,
